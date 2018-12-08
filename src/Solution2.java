c06229f11371fb73255f1657004abbd577883f7b
lmyaxx <1367968283@qq.com>
2018-12-08 18:10:33 +0800

    2.Add Two Numbers

---
+++ src/Solution2.java
@@ -0,0 +1,29 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class Solution2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(-1);
+        ListNode head = pre;
+        int extral=0;
+        int sum=0;
+        while(l1!=null || l2!=null || extral!=0){
+            sum = (l1 != null ? l1.val: 0) + (l2!=null ? l2.val : 0) + extral;
+            ListNode one = new ListNode(sum%10);
+            extral = sum / 10;
+            pre.next = one;
+            pre = one;
+            if(l1!=null){
+                l1 = l1.next;
+            }
+            if(l2!=null){
+                l2 = l2.next;
+            }
+        }
+        pre.next = null;
+        return head.next ;
+    }
+}
