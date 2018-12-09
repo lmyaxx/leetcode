70a1328db29ab62d2cd452159a7901d862f42488
lmyaxx <1367968283@qq.com>
2018-12-09 14:50:00 +0800

    001

---
+++ src/Solution001.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution001 {
+    public int[] twoSum(int[] nums, int target) {
+        int [] result = new int [2];
+        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target - nums[i])){
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                break;
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return result;
+    }
+}
