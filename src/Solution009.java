7f5b446b627bf40f7b3e00cf67e1215368b0180e
lmyaxx <1367968283@qq.com>
2019-05-11 19:55:02 +0800

    add new method for 009

--- src/Solution009.java
+++ src/Solution009.java
@@ -15,4 +15,20 @@ public class Solution009 {
         }
         return true;
     }
+    /*----------------------第二种答案------------------------*/
+    public boolean isPalindrome1(int x) {
+        if(x<0)
+            return false;
+        return revert(x)==x;
+
+    }
+    //可能越界，但越界的一定不是回文Int型数字
+    public int revert(int x){
+        int ans=0;
+        while(x>0){
+            ans=ans*10 + x%10;
+            x=x/10;
+        }
+        return ans;
+    }
 }
