077f56e8bf033720b9c67a2d4163534e97fe17d7
lmyaxx <1367968283@qq.com>
2019-06-24 17:31:42 +0800

    leetcode 027

--- src/Solution027.java
+++ src/Solution027.java
@@ -2,11 +2,8 @@ public class Solution027 {
     public int removeElement(int[] nums, int val) {
         int len=nums.length,count=0;
         for(int i=0;i<len;i++){
-            if(nums[i]==val)
-                continue;
-            else{
+            if(nums[i]!=val)
                 nums[count++]=nums[i];
-            }
         }
         return count;
     }
