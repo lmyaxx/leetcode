bc891b9d5e18757a050194ccaefa15fd709fa2c3
lmyaxx <1367968283@qq.com>
2019-06-24 17:28:32 +0800

    leetcode 027

---
+++ src/Solution027.java
@@ -0,0 +1,13 @@
+public class Solution027 {
+    public int removeElement(int[] nums, int val) {
+        int len=nums.length,count=0;
+        for(int i=0;i<len;i++){
+            if(nums[i]==val)
+                continue;
+            else{
+                nums[count++]=nums[i];
+            }
+        }
+        return count;
+    }
+}
