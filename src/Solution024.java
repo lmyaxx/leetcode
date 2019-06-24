39bb3367f81928be722f6bff7910fcdf929028a6
lmyaxx <1367968283@qq.com>
2019-06-24 15:19:45 +0800

    leetcode 024

---
+++ src/Solution024.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution024 {
+    public ListNode swapPairs(ListNode head) {
+        if(head!=null&&head.next!=null){
+            ListNode rear = head.next;
+            head.next=swapPairs(rear.next);
+            rear.next =head;
+            return rear;
+        }
+        return head;
+    }
+}
