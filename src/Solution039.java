501ac093ff91e0677d3e462113971b0da625de4f
lmyaxx <1367968283@qq.com>
2019-07-13 12:29:12 +0800

    leetcode 039

---
+++ src/Solution039.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution039 {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        int right = candidates.length-1;
+        List<Integer> stack = new ArrayList<>();
+        helper(candidates,right,target,stack,result);
+        return result;
+    }
+
+    private void helper(int[] candidates,int right,int target,List<Integer> stack,List<List<Integer>> result){
+        for(int i=right;i>=0;i--){
+            if(candidates[i]-target==0) {
+                stack.add(candidates[i]);
+                result.add(new ArrayList<>(stack));
+                stack.remove(stack.size()-1);
+            }
+            if(candidates[i]< target){
+                stack.add(candidates[i]);
+                helper(candidates,i,target-candidates[i],stack,result);
+                stack.remove(stack.size()-1);
+            }
+        }
+    }
+}
