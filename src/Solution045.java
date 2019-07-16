de85a43d4ecec548113a035e32e56856e304f88b
lmyaxx <1367968283@qq.com>
2019-07-16 14:11:31 +0800

    leetcode 045

---
+++ src/Solution045.java
@@ -0,0 +1,19 @@
+public class Solution045 {
+    public int jump(int[] nums) {
+        int len = nums.length;
+        if( len==1) return 0;
+        int left=0,right=0;
+        int result=0;
+        while(right<=len){
+            int temp = right;
+            result ++;
+            while(left<=temp){
+                right = Math.max(left + nums[left],right);
+                if(right>=len-1)
+                    return result;
+                left++;
+            }
+        }
+        return result;
+    }
+}
