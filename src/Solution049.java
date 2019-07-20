41ac6d574486f043126a7220972308f6fa96fcd6
lmyaxx <1367968283@qq.com>
2019-07-20 11:58:09 +0800

    leetcode 049

---
+++ src/Solution049.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution049 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> store =  new HashMap<>();
+        for (String s:strs) {
+            char str[] = s.toCharArray();
+            Arrays.sort(str);
+            String sss = String.valueOf(str);
+            if(!store.containsKey(sss)){
+                store.put(sss,new ArrayList<String>());
+            }
+            store.get(sss).add(s);
+        }
+        return new ArrayList<List<String>>(store.values());
+    }
+}
