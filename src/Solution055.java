e1e39b5d219550a5a869de542f020f4f89b948cb
lmyaxx <1367968283@qq.com>
2019-07-21 19:53:52 +0800

    leetcode 055

---
+++ src/Solution055.java
@@ -0,0 +1,16 @@
+public class Solution055 {
+    public boolean canJump(int[] nums) {
+        int len = nums.length;
+        if(len<=1) return true;
+        int left=0,right=0;
+        while(left<=right){
+            int temp=right;
+            for(;left<=temp;left++){
+                right = Math.max(nums[left]+left,right);
+            }
+            if(right>=len-1) return true;
+            if(right<=temp) return false;
+        }
+        return false;
+    }
+}
