506ca65256e4a01567c3ad4e2c49d6824b6a5787
lmyaxx <1367968283@qq.com>
2019-07-21 09:13:14 +0800

    leetcode 053

---
+++ src/Solution053.java
@@ -0,0 +1,15 @@
+public class Solution053 {
+    public int maxSubArray(int[] nums) {
+        if(nums.length==0) return 0;
+        int result=Integer.MIN_VALUE,cul=0;
+        for(int i=0;i<nums.length;i++){
+            if(cul<0){
+                cul = nums[i];
+            }else{
+                cul+=nums[i];
+            }
+            result = Math.max(cul,result);
+        }
+        return result;
+    }
+}
