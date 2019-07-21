7e904b90a0db89b4aec57abd63dafa0cadb37460
lmyaxx <1367968283@qq.com>
2019-07-21 09:00:26 +0800

    leetcode 050

--- src/Solution050.java
+++ src/Solution050.java
@@ -4,4 +4,16 @@ public class Solution050 {
         if(n<0) return myPow(1/x,-(n+1))*1/x;
         return n%2 != 1 ? myPow(x*x,n/2) : x*myPow(x*x,n/2);
     }
+
+    public double myPow1(double x, int n) {
+        if(n<0) return myPow(1/x,-(n+1))*1/x;
+        double result=0;
+        while(n>0){
+            if((n&1)==1)
+                result*=x;
+            x*=x;
+            n=n>>1;
+        }
+        return result;
+    }
 }
