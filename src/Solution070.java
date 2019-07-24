34d6d507a0b9e8c4fd712e3c84497643a3f9f010
lmyaxx <1367968283@qq.com>
2019-07-24 10:13:47 +0800

    leetcode 070

---
+++ src/Solution070.java
@@ -0,0 +1,12 @@
+public class Solution070 {
+    public int climbStairs(int n) {
+        if(n<3) return n;
+        int one=1,two=2,temp;
+        while(n-->2){
+            temp = two+one;
+            one=two;
+            two=temp;
+        }
+        return two;
+    }
+}
