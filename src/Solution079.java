7f561ddb1a3aae91048ba0d7556005ca12f8c900
lmyaxx <1367968283@qq.com>
2019-07-25 18:49:58 +0800

    leetcode 079

---
+++ src/Solution079.java
@@ -0,0 +1,26 @@
+public class Solution079 {
+    public boolean exist(char[][] board, String word) {
+        if(board==null||board.length==0||word.length()==0) return false;
+        int rows = board.length,cols=board[0].length;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(helper(board,word,i,j,0))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean helper(char [][]board,String word,int row,int col,int index){
+        if(index==word.length()) return true;
+        if(row<0||col<0||row>=board.length||col>=board[0].length||board[row][col]!=word.charAt(index))
+            return false;
+        index++;
+        char temp = board[row][col];
+        board[row][col] = '\0';
+        boolean result = helper(board, word, row+1, col, index)||helper(board, word, row, col+1, index)||
+                helper(board, word, row-1, col, index)||helper(board, word, row, col-1, index);
+        board[row][col] = temp;
+        return result;
+    }
+}
