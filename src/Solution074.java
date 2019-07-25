5b19c844b7b6447f42f37321e6afac2707a7ebef
lmyaxx <1367968283@qq.com>
2019-07-25 10:24:22 +0800

    leetcode 074

---
+++ src/Solution074.java
@@ -0,0 +1,14 @@
+public class Solution074 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length==0) return false;
+        int width = matrix[0].length,right=width*matrix.length-1,left=0;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(matrix[mid/width][mid%width]<=target) left=mid+1;
+            else right=mid-1;
+        }
+        if(left<0)
+            return false;
+        return matrix[left/width][left%width] == target;
+    }
+}
