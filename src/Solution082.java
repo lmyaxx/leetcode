790a5d08723de4f6b9535dff78626b5e350c4cc5
lmyaxx <1367968283@qq.com>
2019-07-27 20:22:36 +0800

    leetcode 082

---
+++ src/Solution082.java
@@ -0,0 +1,23 @@
+public class Solution082 {
+    public ListNode deleteDuplicates(ListNode head) {
+//        1->2->3->3->4->4->5
+        ListNode result = new ListNode(-1);
+        result.next=null;
+        ListNode cur=result;
+        int count=1;
+        while(head!=null){
+            if(head.next==null||head.next.val!=head.val){
+                if(count==1){
+                    cur.next=head;
+                    cur = head;
+                }
+                count=1;
+            }else{
+                count++;
+            }
+            head=head.next;
+            cur.next=null;
+        }
+        return result.next;
+    }
+}
