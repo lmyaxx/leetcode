7b43b28f412f22922e8f2f68130bbeab7befa626
lmyaxx <1367968283@qq.com>
2019-08-03 13:55:51 +0800

    leetcode 084

--- src/Solution084.java
+++ src/Solution084.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class Solution084 {
     public int largestRectangleArea(int[] heights) {
         int minIndex=0;
@@ -14,5 +16,30 @@ public class Solution084 {
             result = Math.max((i+1)*heights[minIndex],result);
         }
         return result;
+    }
+
+    public int largestRectangleArea1(int[] height) {
+        int n = height.length, i = 0, max = 0;
+
+        Stack<Integer> s = new Stack<>();
+
+        while (i < n) {
+            // as long as the current bar is shorter than the last one in the stack
+            // we keep popping out the stack and calculate the area based on
+            // the popped bar
+            while (!s.isEmpty() && height[i] < height[s.peek()]) {
+                // tricky part is how to handle the index of the left bound
+                max = Math.max(max, height[s.pop()] * (i - (s.isEmpty() ? 0 : s.peek() + 1)));
+            }
+            // put current bar's index to the stack
+            s.push(i++);
+        }
+
+        // finally pop out any bar left in the stack and calculate the area based on it
+        while (!s.isEmpty()) {
+            max = Math.max(max, height[s.pop()] * (n - (s.isEmpty() ? 0 : s.peek() + 1)));
+        }
+
+        return max;
     }
 }
