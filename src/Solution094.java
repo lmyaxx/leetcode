b4ef19a6eca8abe245df62d7df0ce983463865f5
lmyaxx <1367968283@qq.com>
2019-08-05 08:42:20 +0800

    leetcode 094

---
+++ src/Solution094.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution094 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root,res);
+        return res;
+    }
+    private void helper(TreeNode node,List<Integer> res){
+        if(node==null) return;
+        helper(node.left,res);
+        res.add(node.val);
+        helper(node.right,res);
+    }
+}
