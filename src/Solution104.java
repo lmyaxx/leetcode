f4d145c67d46a043e5898e6f26732d20dc0e30d2
lmyaxx <1367968283@qq.com>
2019-08-06 20:35:56 +0800

    leetcode104

---
+++ src/Solution104.java
@@ -0,0 +1,6 @@
+public class Solution104 {
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+    }
+}
