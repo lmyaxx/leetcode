9cca66935e9a305380454fbcfcffa351c82381e1
lmyaxx <1367968283@qq.com>
2019-08-06 20:32:27 +0800

    leetcode 103

---
+++ src/Solution103.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution103 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        helper(root,result,0);
+        return result;
+    }
+
+    private void helper(TreeNode root,List<List<Integer>> res,int level){
+        if(root==null) return;
+        if(level==res.size()){
+            res.add(new LinkedList<>());
+        }
+        if(level%2==0){
+            res.get(level).add(0,root.val);
+        }else{
+            res.get(level).add(res.get(level).size(),root.val);
+        }
+        helper(root.left,res,level+1);
+        helper(root.right,res,level+1);
+    }
+}
