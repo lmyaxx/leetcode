ed833020c399a9ab7a7c86db75a20dc452fb1d02
lmyaxx <1367968283@qq.com>
2019-08-07 16:40:28 +0800

    leetcode110

---
+++ src/Solution110.java
@@ -0,0 +1,16 @@
+public class Solution110 {
+    public boolean isBalanced(TreeNode root) {
+        return helper(root)!=-1;
+    }
+    private int helper(TreeNode root){
+        if(root==null) return 0;
+        int left = helper(root.left);
+        if(left<0) return -1;
+        int right = helper(root.right);
+        if(right<0) return -1;
+        if(Math.abs(left-right)<=1){
+            return Math.max(left,right)+1;
+        }
+        return -1;
+    }
+}
