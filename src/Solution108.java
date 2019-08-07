e2e6d8f6ffb6eb2d161a4e8558df19a303f7fdce
lmyaxx <1367968283@qq.com>
2019-08-07 14:10:06 +0800

    leetcode108

---
+++ src/Solution108.java
@@ -0,0 +1,14 @@
+public class Solution108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums,0,nums.length-1);
+    }
+
+    private TreeNode helper(int [] nums,int left,int right){
+        if(left>right) return null;
+        int mid=(left+right+1)/2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums,left,mid-1);
+        root.right = helper(nums,mid+1,right);
+        return root;
+    }
+}
