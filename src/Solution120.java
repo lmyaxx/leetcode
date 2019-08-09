6089533f1568612e689f0f098ec8fc7a0f27275d
lmyaxx <1367968283@qq.com>
2019-08-09 13:01:20 +0800

    leetcode120

---
+++ src/Solution120.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class Solution120 {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int rows = triangle.size();
+        for (int i = 1; i < rows; i++) {
+            List<Integer> cur = triangle.get(i);
+            for (int j = 0, len = cur.size(); j < len; j++) {
+                cur.set(j,cur.get(j)+helper(triangle.get(i-1),j));
+            }
+        }
+        int res = Integer.MAX_VALUE;
+        for(Integer one : triangle.get(rows-1)){
+            res= Math.min(res,one);
+        }
+        return res;
+    }
+    private int helper(List<Integer> list,int index){
+        int len=list.size();
+        int res = Integer.MAX_VALUE;
+        if(index-1>=0) res=Math.min(res,list.get(index-1));
+        if(index<len) res = Math.min(res,list.get(index));
+        return res;
+    }
+}
