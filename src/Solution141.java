e4ed7c78517af77b5314a39934f9a7907fae80df
lmyaxx <1367968283@qq.com>
2019-08-15 15:31:24 +0800

    leetcode141

---
+++ src/Solution141.java
@@ -0,0 +1,14 @@
+import Common.ListNode;
+
+public class Solution141 {
+    public boolean hasCycle(ListNode head) {
+        ListNode walker = head,runner = head;
+        while(runner!=null&&runner.next!=null){
+            walker=walker.next;
+            runner=runner.next.next;
+            if(runner==walker)
+                return true;
+        }
+        return false;
+    }
+}
