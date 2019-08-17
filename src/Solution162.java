8d45a8e9dc41b25fe7c6364d454fec6ae033e779
lmyaxx <1367968283@qq.com>
2019-08-17 11:08:19 +0800

    leetcode162

---
+++ src/Solution162.java
@@ -0,0 +1,12 @@
+public class Solution162 {
+    public int findPeakElement(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]<nums[i]){
+                continue;
+            }else{
+                return i-1;
+            }
+        }
+        return nums.length-1;
+    }
+}
