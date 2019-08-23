a157b3cd237eef4996b75eb0b5ef323454d0d414
lmyaxx <1367968283@qq.com>
2019-08-23 11:31:06 +0800

    leetcode207

--- src/Solution207.java
+++ src/Solution207.java
@@ -1,15 +1,71 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Solution207 {
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> li = new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
-            li.add(new ArrayList<>());
+        ArrayList[] graph = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList();
         }
-        for(int i= 0,len=prerequisites.length;i<len;i++){
-//            li.add()
+        for (int[] prerequisite : prerequisites) {
+            graph[prerequisite[1]].add(prerequisite[0]);
         }
-        return false;
+        boolean visited[] = new boolean[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(graph, visited, i)) {
+                return false;
+            }
+        }
+        return true;
     }
+
+    private boolean dfs(ArrayList[] graph, boolean[] visited, int course) {
+        if (visited[course]) {
+            return false;
+        } else {
+            visited[course] = true;
+        }
+        for (int i = 0; i < graph[course].size(); i++) {
+            if (!dfs(graph, visited, (int) graph[course].get(i))) {
+                return false;
+            }
+        }
+        visited[course] = false;
+        return true;
+    }
+
+    public boolean bfs(int numCourses, int[][] prerequisites) {
+        ArrayList[] graph = new ArrayList[numCourses];
+        int degree [] = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList();
+        }
+        for (int[] prerequisite : prerequisites) {
+            graph[prerequisite[1]].add(prerequisite[0]);
+            degree[prerequisite[0]]++;
+        }
+        int count = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0;i<degree.length;i++){
+            if(degree[i]==0){
+                queue.offer(i);
+            }
+        }
+        while(!queue.isEmpty()){
+            int course = queue.poll();
+            degree[course]--;
+            count++;
+            for(int i=0;i<graph[course].size();i++){
+                int next = (int)graph[course].get(i);
+                degree[next]--;
+                if(degree[next]==0){
+                    queue.offer(next);
+                }
+            }
+        }
+        return count==numCourses;
+    }
+
+
+
 }
