1468d661a185499b61cd63d9b240747b576bf5c9
lmyaxx <1367968283@qq.com>
2019-08-25 10:36:04 +0800

    leetcode208

---
+++ src/Common/Trie.java
@@ -0,0 +1,48 @@
+package Common;
+
+
+
+public class Trie {
+    private TrieNode root;
+
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+       TrieNode cur = root;
+       for(int i=0;i<word.length();i++){
+           char ch = word.charAt(i);
+           if(cur.children[ch-'a']==null){
+               cur.children[ch-'a'] = new TrieNode();
+           }
+           cur = cur.children[ch-'a'];
+       }
+       cur.isWord = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            if(cur.children[ch-'a']==null) return false;
+            cur = cur.children[ch-'a'];
+        }
+        return cur.isWord;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for(int i=0;i<prefix.length();i++){
+            char ch = prefix.charAt(i);
+            if(cur.children[ch-'a']==null) return false;
+            cur = cur.children[ch-'a'];
+        }
+        return true;
+    }
+}
---
+++ src/Common/TrieNode.java
@@ -0,0 +1,7 @@
+package Common;
+
+public class TrieNode {
+    public boolean isWord;
+    public TrieNode[] children = new TrieNode[26];
+    public TrieNode() {}
+}
