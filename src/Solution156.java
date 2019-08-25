676cba50cae62dca93f2adc53b1ea117aea1f7ce
lmyaxx <1367968283@qq.com>
2019-08-25 10:37:06 +0800

    leetcode156

---
+++ src/Solution156.java
@@ -0,0 +1,22 @@
+import Common.TreeNode;
+
+public class Solution156 {
+
+    //wait for judge
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root==null||root.left==null) return root;
+        return helper(root.left,root.right,root);
+    }
+
+    private TreeNode helper(TreeNode root,TreeNode left,TreeNode right){
+        TreeNode nextRoot = root.left;
+        TreeNode nextleft = root.right;
+        right.right=null;
+        root.left =left;
+        root.right=right;
+        if(nextRoot!=null)
+            return helper(nextRoot,nextleft,root);
+        else
+            return root;
+    }
+}
