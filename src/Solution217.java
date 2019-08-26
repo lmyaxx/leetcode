534f7f59d808d3a4647304274d81bf8f2ecc8d27
lmyaxx <1367968283@qq.com>
2019-08-26 21:46:21 +0800

    leetcode217

---
+++ src/Solution217.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution217 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int len = nums.length;
+        for(int num:nums){
+            if(!set.add(num)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
