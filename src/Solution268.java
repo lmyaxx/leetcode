3e4857ba3c92ed8ad3fa7a1f1e140e3f2b30b0ff
lmyaxx <1367968283@qq.com>
2019-09-15 10:55:22 +0800

    leetcode268

---
+++ src/Solution268.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+
+public class Solution268 {
+    public int missingNumber(int[] nums) {
+        int res = nums.length;
+        for(int i=0;i<nums.length;i++){
+            res=res^i^nums[i];
+        }
+        return res;
+    }
+}
