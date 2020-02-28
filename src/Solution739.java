4ee7da9fcfdf5395455d48538ce403d959367c9f
lmyaxx <1367968283@qq.com>
2020-02-28 17:00:56 +0800

    leetcode739

---
+++ src/Solution739.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+/**
+ * @Author : Chad li
+ * @Date : 2020/2/28 16:33
+ * @Version 1.0
+ */
+public class Solution739 {
+    public int[] dailyTemperatures(int[] T) {
+        int[] res = new int[T.length];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(T.length -1);
+        for(int i = res.length - 2; i>=0;i--){
+            while(!stack.isEmpty() &&T[i] >= T[stack.peek()]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                res[i] = 0;
+            } else{
+                res[i] = stack.peek() - i;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
