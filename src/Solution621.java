f10eb31b9153ced2a905e15f5eb55076970e89ab
lmyaxx <1367968283@qq.com>
2020-03-01 09:41:39 +0800

    leetcode621

---
+++ src/Solution621.java
@@ -0,0 +1,27 @@
+import javafx.util.Pair;
+
+import java.util.*;
+
+/**
+ * @Author : Chad li
+ * @Date : 2020/2/29 14:10
+ * @Version 1.0
+ */
+public class Solution621 {
+    public int leastInterval(char[] tasks, int n) {
+        int[] count  = new int[26];
+        int total = tasks.length;
+        int res = 0 ;
+        for(char ch:tasks){
+            count[ch-'A'] ++;
+        }
+        Arrays.sort(count);
+        int i = 24;
+        while(i>=0&&count[i]==count[i+1]){
+            i--;
+        }
+        return Math.max(tasks.length, (count[25] - 1)*(n+1) + 25- i);
+    }
+
+
+}
