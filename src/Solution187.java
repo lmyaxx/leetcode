1d4f7b31eadb52ab78b0857e1cc719b3c265762f
lmyaxx <1367968283@qq.com>
2020-03-05 10:52:56 +0800

    leetcode187

---
+++ src/Solution187.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @Author : Chad li
+ * @Date : 2020/3/5 10:45
+ * @Version 1.0
+ */
+public class Solution187 {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set seen  = new HashSet<>();
+        Set repeated = new HashSet();
+        for(int i=0; i+9<s.length();i++){
+            String str = s.substring(i,i+10);
+            if(seen.contains(str)){
+                repeated.add(str);
+            }else{
+                seen.add(str);
+            }
+        }
+        return new ArrayList<>(repeated);
+    }
+}
