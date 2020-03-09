d36355957cfe7f0ce7df6bde3ba08e92c65ccd08
lmyaxx <1367968283@qq.com>
2020-03-09 17:52:28 +0800

    leetcode199

---
+++ src/Solution199.java
@@ -0,0 +1,41 @@
+import Common.TreeNode;
+import jdk.nashorn.api.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @Author : Chad li
+ * @Date : 2020/3/9 16:50
+ * @Version 1.0
+ */
+public class Solution199 {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int count = queue.size();
+            while(true){
+                TreeNode cur = queue.poll();
+                count--;
+                if(cur.left!=null){
+                    queue.offer(cur.left);
+                }
+                if(cur.right!=null){
+                    queue.offer(cur.right);
+                }
+                if(count==0){
+                    res.add(cur.val);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
