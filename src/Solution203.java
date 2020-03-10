60a2cc8d8ca5a48f63b18664192c9fd7eccab8c0
lmyaxx <1367968283@qq.com>
2020-03-10 11:16:20 +0800

    leetcode203

---
+++ src/Solution203.java
@@ -0,0 +1,27 @@
+import Common.ListNode;
+import Common.TreeNode;
+
+/**
+ * @Author : Chad li
+ * @Date : 2020/3/10 11:04
+ * @Version 1.0
+ */
+public class Solution203 {
+    public ListNode removeElements(ListNode head, int val) {
+        while(head!=null&&head.val == val){
+            head = head.next;
+        }
+        if(head==null){
+            return head;
+        }
+        ListNode last = head, cur = head.next;
+        while(cur!=null){
+            if(cur.val!=val){
+                last = cur;
+            }
+            last.next = cur.next;
+            cur = cur.next;
+        }
+        return head;
+    }
+}
