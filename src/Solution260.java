706b3edfdc32f5f99f1a0f9c09abd4922ed76bf5
lmyaxx <1367968283@qq.com>
2020-03-18 09:28:28 +0800

    leetcode260

---
+++ src/Solution260.java
@@ -0,0 +1,23 @@
+/**
+ * @Author : Chad li
+ * @Date : 2020/3/18 9:11
+ * @Version 1.0
+ */
+public class Solution260 {
+    public int[] singleNumber(int[] nums) {
+        int diff = 0;
+        for(int num:nums){
+            diff ^= num;
+        }
+        diff &= (-diff);
+        int[] res = new int[]{0,0};
+        for(int num:nums){
+            if((diff&num)==diff){
+                res[0] ^=num;
+            }else{
+                res[1] ^=num;
+            }
+        }
+        return res;
+    }
+}
