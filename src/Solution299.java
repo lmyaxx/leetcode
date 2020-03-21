46dbe9a8e291f67c278a8e919e4954e55e750a89
lmyaxx <1367968283@qq.com>
2020-03-21 11:22:08 +0800

    leetcode299

---
+++ src/Solution299.java
@@ -0,0 +1,24 @@
+/**
+ * @Author : Chad li
+ * @Date : 2020/3/21 11:05
+ * @Version 1.0
+ */
+public class Solution299 {
+    public String getHint(String secret, String guess) {
+        int[] countSecret = new int[10];
+        int[] countGuess = new int[10];
+        int A = 0,B = 0;
+        for(int i=0;i<secret.length();i++){
+            if(secret.charAt(i)==guess.charAt(i)){
+                A++;
+            }else{
+                countSecret[secret.charAt(i)-'0']++;
+                countGuess[guess.charAt(i)-'0']++;
+            }
+        }
+        for(int i=0;i<10;i++){
+            B+=Math.min(countGuess[i],countSecret[i]);
+        }
+        return A+"A"+B+"B";
+    }
+}
