3b4e4c8212175374813463d4b545bd569acd65fd
lmyaxx <1367968283@qq.com>
2020-03-29 21:00:15 +0800

    leetcode330

---
+++ src/Solution330.java
@@ -0,0 +1,21 @@
+/**
+ * @Author : Chad li
+ * @Date : 2020/3/29 20:08
+ * @Version 1.0
+ */
+public class Solution330 {
+    public int minPatches(int[] nums, int n) {
+        long missed = 1;
+        int count=0,i=0;
+        while(missed<=n){
+            if(i<nums.length&&nums[i]<=missed){
+                missed += nums[i++];
+            }else{
+                count++;
+                missed += missed;
+            }
+        }
+        return count;
+    }
+
+}
