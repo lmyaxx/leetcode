a13d360efc90aaaa7dcaae07f545d21227d01b84
lmyaxx <1367968283@qq.com>
2020-04-27 22:07:17 +0800

    leetcode472

--- src/Solution472.java
+++ src/Solution472.java
@@ -41,4 +41,68 @@ public class Solution472 {
         }
         return dp[word.length()];
     }
+
+    public List<String> findAllConcatenatedWordsInADict1(String[] words) {
+        List<String> res = new ArrayList<String>();
+        if (words == null || words.length == 0) {
+            return res;
+        }
+        TrieNode root = new TrieNode();
+        // construct Trie tree
+        for (String word : words) {
+            if (word.length() == 0) {
+                continue;
+            }
+            addWord(root,word);
+        }
+        // test word is a concatenated word or not
+        for (String word : words) {
+            if (word.length() == 0) {
+                continue;
+            }
+            if (testCanForm(word.toCharArray(), 0, root, 0)) {
+                res.add(word);
+            }
+        }
+        return res;
+    }
+
+    private boolean testCanForm(char[] word, int idx, TrieNode root, int count){
+        TrieNode cur = root;
+        for(int i=idx;i<word.length;i++){
+            if(cur.children[word[i]-'a']==null){
+                return false;
+            }
+            cur = cur.children[word[i]-'a'];
+            if(cur.isWord){
+                if(i == word.length-1){
+                    return count>=1;
+                }
+                if(testCanForm(word,i+1,root,count+1)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private void addWord(TrieNode root, String word){
+        for (char ch : word.toCharArray()){
+            if(root.children[ch-'a']==null){
+                root.children[ch-'a'] = new TrieNode();
+            }
+            root = root.children[ch-'a'];
+        }
+        root.isWord = true;
+    }
+
+    class TrieNode{
+        TrieNode[] children;
+        boolean isWord;
+        public TrieNode(){
+            isWord = false;
+            children = new TrieNode[26];
+        }
+    }
 }
