91c12d699d291d4b9c6e92ef57b6a41e678063ba
lmyaxx <1367968283@qq.com>
2020-05-06 14:43:51 +0800

    leetcode517

---
+++ src/Solution517.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+/**
+ * @Author : Chad li
+ * @Date : 2020/5/4 9:18
+ * @Version 1.0
+ */
+public class Solution517 {
+    public int findMinMoves(int[] machines) {
+        int total = 0, avg = 0, len = machines.length;
+        for(int num:machines){
+            total+=num;
+        }
+        if(total%len!=0){
+            return -1;
+        }
+        avg = total / len;
+        int  cnt = 0, max = 0;
+        for(int load: machines){
+            cnt += load-avg; //load-avg is "gain/lose"
+            max = Math.max(Math.max(max, Math.abs(cnt)), load-avg);
+        }
+        return max;
+    }
+}
