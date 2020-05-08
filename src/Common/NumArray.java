352689ece5ab2ff9f9196ad1158c793e7a414b46
lmyaxx <1367968283@qq.com>
2020-05-08 10:45:05 +0800

    leetcode187

---
+++ src/Common/NumArray.java
@@ -0,0 +1,22 @@
+package Common;
+
+/**
+ * @Author : Chad li
+ * @Date : 2020/5/8 10:33
+ * @Version 1.0
+ */
+public class NumArray {
+
+    private long[] pre;
+
+    public NumArray(int[] nums) {
+        pre = new long[nums.length+1];
+        for(int i=1;i<=nums.length;i++){
+            pre[i] += nums[i-1] + pre[i-1];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return (int)(pre[j+1] - pre[i]);
+    }
+}
---
+++ src/Solution303.java
@@ -0,0 +1,16 @@
+import Common.NumArray;
+import org.junit.Test;
+
+/**
+ * @Author : Chad li
+ * @Date : 2020/5/8 10:32
+ * @Version 1.0
+ */
+public class Solution303 {
+    @Test
+    public void test_Solution(){
+        int[] nums = new int[]{1,2,3,5,7,3,-2};
+        NumArray numArray = new NumArray(nums);
+        System.out.println(numArray.sumRange(0,4));
+    }
+}
