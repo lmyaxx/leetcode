6b28bf1aacf7589d03d9426ae7db5a6dc232081a
lmyaxx <1367968283@qq.com>
2020-06-17 09:16:59 +0800

    leetcode1014

---
+++ src/Solution1014.java
@@ -0,0 +1,11 @@
+public class Solution1014 {
+    public int maxScoreSightseeingPair(int[] A) {
+        int left = A[0] + 0;
+        int res = 0;
+        for(int j=1;j<A.length;j++){
+            res = Math.max(res,left+A[j]-j);
+            left = Math.max(left,A[j]+j);
+        }
+        return res;
+    }
+}
