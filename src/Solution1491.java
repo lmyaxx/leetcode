0ac4020b76e7e5b6227a23ab3bea7cde15db7947
lmyaxx <1367968283@qq.com>
2020-07-11 10:14:44 +0800

    leetcode1491

---
+++ src/Solution1491.java
@@ -0,0 +1,12 @@
+public class Solution1491 {
+    public double average(int[] salary) {
+        double min = Integer.MAX_VALUE, max =Integer.MIN_VALUE;
+        double ans = 0;
+        for(int s: salary){
+            min = Math.min(min,s);
+            max = Math.max(max,s);
+            ans += s;
+        }
+        return (ans-min-max) / (salary.length - 2);
+    }
+}
