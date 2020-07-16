d8ef11046c6dc97cec74ffe1f3f6b7dbcc99c6b5
lmyaxx <1367968283@qq.com>
2020-07-16 18:31:43 +0800

    leetcode785

---
+++ src/Solution785.java
@@ -0,0 +1,31 @@
+public class Solution785 {
+    public boolean isBipartite(int[][] graph) {
+        int[] group = new int [graph.length];
+        for(int i=0;i<graph.length;i++){
+            if(group[i]==0){
+                group[i] = 1;
+                if(!dfs(graph,i,group)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean dfs(int[][] graph, int root,int[] group){
+        int[] adj = graph[root];
+        for(int i=0;i<adj.length;i++){
+            if(group[adj[i]]==0){
+                group[adj[i]] = group[root] % 2 + 1;
+                if(!dfs(graph,adj[i],group)){
+                    return false;
+                }
+            }else{
+                if(group[adj[i]]==group[root]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
