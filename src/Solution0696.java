98cda792af72d84a37b0c6d5afc5eec5b3ffc8c9
lmyaxx <1367968283@qq.com>
2020-08-11 08:50:54 +0800

    leetcode696

---
+++ src/Solution0696.java
@@ -0,0 +1,25 @@
+public class Solution0696 {
+    public int countBinarySubstrings(String s) {
+        int res = 0, i =0;
+        int count1 = 0, count2=0;
+        char last= '0';
+        if(s.charAt(0)=='0'){
+            last = '1';
+        }
+        while(i<s.length()){
+            while(i<s.length()&&s.charAt(i) != last){
+                i++;
+                count1++;
+            }
+            res += Math.min(count1,count2);
+            count2 = count1;
+            count1 = 0;
+            if(last=='0'){
+                last = '1';
+            }else{
+                last = '0';
+            }
+        }
+        return res;
+    }
+}
