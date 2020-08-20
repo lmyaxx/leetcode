fc39c1b92ee83e6db8df1d6e558ea7585a58f382
lmyaxx <1367968283@qq.com>
2020-08-20 20:05:42 +0800

    leetcode1546

---
+++ src/Solution1546.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+public class Solution1546 {
+    public int maxNonOverlapping(int[] nums, int target) {
+        int count = 0, sum = 0;
+        HashSet<Integer> set = new HashSet<Integer>();
+        set.add(0);
+        for(int i=0; i< nums.length ; i++){
+            sum += nums[i];
+            if(set.contains(sum - target)){
+                count ++;
+                set.clear();
+                set.add(0);
+                sum = 0;
+            }else{
+                set.add(sum);
+            }
+        }
+        return count;
+    }
+}
