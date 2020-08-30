6378f75d69fd771c07b2c83bffe2aee8fee3d573
lmyaxx <1367968283@qq.com>
2020-08-30 18:47:50 +0800

    leetcode1566

---
+++ src/Solution1566.java
@@ -0,0 +1,16 @@
+public class Solution1566 {
+    public boolean containsPattern(int[] arr, int m, int k) {
+        int len = arr.length, count = 0, total = m * k;
+        for(int i=0; i + m < len; i++ ){
+            if(arr[i]!=arr[i+m]){
+                count = 0;
+            }else {
+                count ++;
+                if(count>=total-m){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
