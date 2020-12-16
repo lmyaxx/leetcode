79eb35ba5e762512e650c9c2aeb973ec2cdec0e0
lmyaxx <1367968283@qq.com>
2020-12-16 21:57:55 +0800

    leetcode0290

--- src/solution201_400/Solution290.java
+++ src/solution201_400/Solution290.java
@@ -26,4 +26,26 @@ public class Solution290 {
         }
         return true;
     }
+
+    public boolean wordPattern1(String pattern, String str) {
+        String[] words = str.split(" +");
+        if(words.length!=pattern.length()){
+            return false;
+        }
+        Map<Character,String> map1 = new HashMap<>();
+        Map<String,Character> map2 = new HashMap<>();
+        for(int i = 0; i < pattern.length(); i++){
+            char ch = pattern.charAt(i);
+            String word = words[i];
+            if(map1.containsKey(ch) && map2.containsKey(word) && map1.get(ch).equals(word) && map2.get(word).equals(ch)){
+
+            }else if(!map1.containsKey(ch)&&!map2.containsKey(word)){
+                map1.put(ch, word);
+                map2.put(word,ch);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
 }
